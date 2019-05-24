df5b4b5aa086e0a5c8a590156b558e6b8fae0ad9
Erin Sheldon <erin.sheldon@gmail.com>
2019-05-24 12:36:29 -0400

    remove ups stuff

--- setup.py
+++ setup.py
@@ -1,32 +1,10 @@
 from distutils.core import setup,Extension
-import distutils.sysconfig
-from os.path import join as path_join
-import os,sys
 
-main_libdir=distutils.sysconfig.get_python_lib()
-pylib_install_subdir = main_libdir.replace(distutils.sysconfig.PREFIX+os.sep,'')
-
-if not os.path.exists('ups'):
-    os.mkdir('ups')
-tablefile=open('ups/columns.table','w')
-tab="""
-setupOptional("python")
-setupOptional("numpydb")
-setupOptional("esutil")
-envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)
-""" % pylib_install_subdir 
-
-tablefile.write(tab)
-tablefile.close()
-
-data_files=[]
-data_files.append( ('ups',[path_join('ups','columns.table')] ) )
 
 # data_files copies the ups/esutil.table into prefix/ups
-setup(name='columns',
-      description='A simple, efficient, pythonic column database',
-      packages=['columns'],
-      data_files=data_files)
-
-
-
+setup(
+    name='columns',
+    version='0.1.0',
+    description='A simple, efficient, pythonic column database',
+    packages=['columns'],
+)
