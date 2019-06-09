e444da9f8c338513c741412e367673ae34cf9fb1
Erin Sheldon <erin.sheldon@gmail.com>
2019-06-09 10:23:26 -0400

    rename package to pycolumns

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup,Extension
 
 # data_files copies the ups/esutil.table into prefix/ups
 setup(
-    name='columns',
+    name='pycolumns',
     version='0.1.0',
     description='A simple, efficient, pythonic column database',
-    packages=['columns'],
+    packages=['pycolumns'],
 )
